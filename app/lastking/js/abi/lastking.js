a062babd3d66cd015367838277233530100dc25b
S315 <s315clubs@outlook.com>
2024-06-29 21:11:08 +0800

    Add files via upload

---
+++ app/lastking/js/abi/lastking.js
@@ -0,0 +1,522 @@
+var lastking_abi = [
+			{
+				"inputs": [],
+				"stateMutability": "nonpayable",
+				"type": "constructor"
+			},
+			{
+				"inputs": [],
+				"name": "burnRate",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "game",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "issue",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "unitAmount",
+						"type": "uint256"
+					},
+					{
+						"internalType": "address",
+						"name": "winningUser",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "winningBlockNumber",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "delayBlockNumber",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "startBlockNumber",
+						"type": "uint256"
+					},
+					{
+						"internalType": "bool",
+						"name": "status",
+						"type": "bool"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "getDelayBlockNumber",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "getOrderHistory",
+				"outputs": [
+					{
+						"components": [
+							{
+								"internalType": "uint256",
+								"name": "issue",
+								"type": "uint256"
+							},
+							{
+								"internalType": "address",
+								"name": "user",
+								"type": "address"
+							},
+							{
+								"internalType": "uint256",
+								"name": "amount",
+								"type": "uint256"
+							},
+							{
+								"internalType": "uint256",
+								"name": "blockNumber",
+								"type": "uint256"
+							}
+						],
+						"internalType": "struct LastKing.Order[10]",
+						"name": "",
+						"type": "tuple[10]"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "getPoolBalance",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "getTimeLoop",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "getTimes",
+				"outputs": [
+					{
+						"internalType": "uint256[]",
+						"name": "",
+						"type": "uint256[]"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "user",
+						"type": "address"
+					}
+				],
+				"name": "getUserOrders",
+				"outputs": [
+					{
+						"components": [
+							{
+								"internalType": "uint256",
+								"name": "issue",
+								"type": "uint256"
+							},
+							{
+								"internalType": "address",
+								"name": "user",
+								"type": "address"
+							},
+							{
+								"internalType": "uint256",
+								"name": "amount",
+								"type": "uint256"
+							},
+							{
+								"internalType": "uint256",
+								"name": "blockNumber",
+								"type": "uint256"
+							}
+						],
+						"internalType": "struct LastKing.Order[]",
+						"name": "",
+						"type": "tuple[]"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "getWinnerHistory",
+				"outputs": [
+					{
+						"components": [
+							{
+								"internalType": "uint256",
+								"name": "issue",
+								"type": "uint256"
+							},
+							{
+								"internalType": "address",
+								"name": "user",
+								"type": "address"
+							},
+							{
+								"internalType": "uint256",
+								"name": "amount",
+								"type": "uint256"
+							},
+							{
+								"internalType": "uint256",
+								"name": "blockNumber",
+								"type": "uint256"
+							}
+						],
+						"internalType": "struct LastKing.Winner[]",
+						"name": "",
+						"type": "tuple[]"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "launch",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"name": "orderHistory",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "issue",
+						"type": "uint256"
+					},
+					{
+						"internalType": "address",
+						"name": "user",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "blockNumber",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "orderRate",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "renounceOwnership",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "_winningRate",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "_burnRate",
+						"type": "uint256"
+					}
+				],
+				"name": "setRate",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "_timeInterval",
+						"type": "uint256"
+					}
+				],
+				"name": "setTimeInterval",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256[]",
+						"name": "_times",
+						"type": "uint256[]"
+					}
+				],
+				"name": "setTimes",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "_unitAmount",
+						"type": "uint256"
+					}
+				],
+				"name": "setUnitAmount",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "startGame",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "started",
+				"outputs": [
+					{
+						"internalType": "bool",
+						"name": "",
+						"type": "bool"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "stopGame",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "timeInterval",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"name": "times",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "unitAmount",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"name": "userOrders",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "issue",
+						"type": "uint256"
+					},
+					{
+						"internalType": "address",
+						"name": "user",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "blockNumber",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"name": "winners",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "issue",
+						"type": "uint256"
+					},
+					{
+						"internalType": "address",
+						"name": "user",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "blockNumber",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "winningRate",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "withdrawETH",
+				"outputs": [],
+				"stateMutability": "payable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "_tokenAddress",
+						"type": "address"
+					}
+				],
+				"name": "withdrawToken",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			}
+		];
---
+++ app/lastking/js/game.js
@@ -0,0 +1,114 @@
+window.game = {
+	checkAccount:0,
+	browser:"https://bscscan.com",
+	// provider:"https://bsc-dataseed1.binance.org",
+	userAddress:false,
+	tokenAddress:"0xE3762BE69030CC0E96B9388F5AB696dc2eE99fbe",
+	tokenDecimals:18,
+	gameAddress:"0xc1229C30e2D2BcEa3138b624daF9d0cfC34b31E5",
+	currentBlock:0,
+	connect(callback) {
+		
+		this.checkAccount++;
+		if(this.checkAccount >= 10) {
+			callback && callback(false);
+			return;
+		}
+		
+		if(typeof(web3) == "undefined") {
+			setTimeout(()=>{
+				this.connect(callback);
+			}, 100);
+			return;
+		}
+		
+		if(typeof(this.metaMask) == "undefined") {
+			this.metaMask = new Web3(web3.currentProvider);
+			this.metaMask.utils.hexToNumber = function(number) {
+				if(number == undefined) {
+					return 0x00;
+				}
+				return this.toBN(number).toString(10);
+			}
+			this.BN = this.metaMask.utils.BN;
+		}
+		
+		web3.currentProvider.enable().then(function(address) {
+			window.game.userAddress = address[0];
+			callback && callback(window.game.userAddress);
+		}).catch(function() {
+			window.game.userAddress = false;
+			callback && callback(window.game.userAddress);
+		});
+		
+	},
+	send() {
+		var lastking = new this.metaMask.eth.Contract(lastking_abi, this.gameAddress);
+		return lastking.methods.launch().send({from:this.userAddress});
+	},
+	approve() {
+		var token = new this.metaMask.eth.Contract(erc20_abi, this.tokenAddress);
+		return token.methods.approve(this.gameAddress, "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").send({from:this.userAddress});
+	},
+	async allowance() {
+		var token = new this.metaMask.eth.Contract(erc20_abi, this.tokenAddress);
+		var allowance = new this.BN(await token.methods.allowance(this.userAddress, this.gameAddress).call());
+		
+		var gameParameter = await this.gameData();
+		var unitAmount = new this.BN(gameParameter.unitAmount);
+		
+		return {
+			allowanceAmount:allowance, 
+			unitAmount:unitAmount
+		};
+	},
+	async balance(walletAddress) {
+		var token = new this.metaMask.eth.Contract(erc20_abi, this.tokenAddress);
+		var balance = new this.BN(await token.methods.balanceOf(walletAddress).call());
+		return balance;
+	},
+	getCurrentNumber:async function() {
+		
+		if(this.currentBlock == 0) {
+			var currentBlock = await this.metaMask.eth.getBlockNumber();
+			while(true) {
+				if(currentBlock.account_names == undefined) {
+					break;
+				}
+				currentBlock = currentBlock.account_names;
+			}
+			
+			this.currentBlock = parseInt(currentBlock);
+		} else {
+			
+			var currentBlock = this.currentBlock+1;
+			var block = await this.metaMask.eth.getBlock(currentBlock);
+			if(block != undefined && block != null) {
+				this.currentBlock = parseInt(currentBlock);
+			}
+
+		}
+		
+		return this.currentBlock;
+	},
+	async gameData() {
+		var lastking = new this.metaMask.eth.Contract(lastking_abi, this.gameAddress);
+		var gameParameter = await lastking.methods.game().call();
+		
+		gameParameter.currentNumber = await this.getCurrentNumber();
+		gameParameter.balanceAmount = await lastking.methods.getPoolBalance().call();
+		gameParameter.orderHistory  = await lastking.methods.getOrderHistory().call();
+		gameParameter.winnerHistory = await lastking.methods.getWinnerHistory().call();
+		
+		return gameParameter;
+	},
+	// async allowance() {
+	// 	var token = new this.metaMask.eth.Contract(erc20_abi, this.tokenAddress);
+	// 	var allowance = new this.BN(await token.methods.allowance(this.userAddress, this.gameAddress).call());
+	// 	var balance = new this.BN(await token.methods.balanceOf(this.userAddress).call());
+	// 	return {
+	// 		allowance:allowance, 
+	// 		balance:balance,
+	// 	};
+	// }
+}
